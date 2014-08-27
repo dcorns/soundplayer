7081e49d14798b2ef2981035ba91a444eaf20772
Dale Corns <drcorns@hotmail.com>
2014-08-27 14:06:27 -0700

    add an express server

---
+++ server.js
@@ -0,0 +1,29 @@
+//server.js--server side entry code
+'use strict';
+//Packages
+var express = require('express');
+var app = express();
+
+//Static page directory and client side code location settings
+app.use(express.static(__dirname + '/'));
+
+//set environment variables
+var port = process.env.PORT || 8080;
+
+//Pre routing
+var router = express.Router();
+
+//Do this for every route/request
+router.use(function(req, res, next) {
+  console.log('There was a server request');
+  next(); //go on to specific route
+});
+
+// display landing page
+app.get('/',function(req, res) {
+  res.set('Status', '200');
+  res.render('index.html');
+});
+
+app.listen(port);
+console.log('Listening on port ' + port);
