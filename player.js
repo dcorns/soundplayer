192e582454fba84d4f9daa7da384976fe5d9a3e7
Dale Corns <drcorns@hotmail.com>
2014-08-28 19:53:09 -0700

    add some buttons

--- player.js
+++ player.js
@@ -1,10 +1,13 @@
-function player() {
-  var playlist = [];
-  var playing = {};
-  var controls = {};
-  var ui = {};
 
-  function makeUi(){
+function makeplayer() {
+  var player = {
+    playlist: [],
+    playing: {},
+    controls: {},
+    ui: {}
+    };
+  (function () {
+    var ui = player.ui;
     ui.player = document.getElementById('player');
     ui.currentTrack = document.getElementById('currentTrack');
     ui.npImg = document.getElementById('npImg');
@@ -12,5 +15,38 @@ function player() {
     ui.musicFile = document.getElementById('musicFile');
     ui.playBar = document.getElementById('playBar');
     ui.playList = document.getElementById('playList');
-  }
+
+    var controls = player.controls;
+    controls.nextTrack = document.getElementById('nextTrack');
+    controls.nextTrack.onclick = function(){controls.clicked(this)};
+    controls.pauseTrack = document.getElementById('pauseTrack');
+    controls.pauseTrack.onclick = function(){controls.clicked(this)};
+    controls.playTrack = document.getElementById('playTrack');
+    controls.playTrack.onclick = function(){controls.clicked(this)};
+    controls.prevTrack = document.getElementById('prevTrack');
+    controls.prevTrack.onclick = function(){controls.clicked(this)};
+    controls.stopTrack = document.getElementById('stopTrack');
+    controls.stopTrack.onclick = function(){controls.clicked(this)};
+
+    controls.clicked = function (e){
+      var id = e.id;
+      switch (id){
+        case 'playTrack':
+          ui.currentTrack.play();
+          break;
+        case 'pauseTrack':
+          ui.currentTrack.pause();
+          break;
+        default:
+          alert(id+'pressed');
+          break;
+      }
+    }
+  })();
+
+
+
+return player;
 }
+var player = makeplayer();
+
