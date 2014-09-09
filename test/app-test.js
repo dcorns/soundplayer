2377e5581501fef5b34cd710927453ad3950be63
Dale Corns <drcorns@hotmail.com>
2014-09-09 03:17:04 -0700

    add empty server test file

--- test/app-test.js
+++ test/app-test.js
@@ -1,10 +1,1 @@
 'use strict';
-
-var expect = require('chai').expect;
-var List = require('../app.js');
-
-describe('app tests', function(){
-  it ('has a server function',function(){
-    expect(true).to.be.true;
-  });
-});
