60f7c7e2283f2a48a60203df3fbab8237800e2b9
Dale Corns <drcorns@hotmail.com>
2014-09-10 01:59:35 -0700

    add jshintrc

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,10 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all: ['Gruntfile.js','app.js']
+      all: ['Gruntfile.js','app.js'],
+      options: {
+        jshintrc: true
+      }
     }
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
