d030eb46d3ad6ce3f3136047daa058aced7506a6
Dale Corns <drcorns@hotmail.com>
2014-09-16 13:12:55 -0700

    add some server tests

--- test/casperTests/server_test.js
+++ test/casperTests/server_test.js
@@ -1,9 +1,14 @@
 'use strict';
 /*global casper*/
 
-casper.test.begin('Testing the server', 1, function suite(test) {
+casper.test.begin('Testing the server', 3, function suite(test) {
   casper.start('http://localhost:8080/index.html', function() {
     test.assertHttpStatus(200,'Has 200 status');
+    //Element Counts
+    test.assertElementCount('div',2,'Testing div count');
+    //Test Title text
+    test.assertEvalEqual(function(){
+      return __utils__.findOne('title').textContent;},'World Player');
     /*test.assertExists('h1.page-title');
     test.assertSelectorHasText('h1.page-title', 'Hello');
     test.assertVisible('footer');*/
