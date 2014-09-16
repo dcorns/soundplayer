79c6b0eff032d62c49423c9e3e2700fdab08a90b
Dale Corns <drcorns@hotmail.com>
2014-09-16 12:37:16 -0700

    add express and watch to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,10 +16,48 @@ module.exports = function(grunt) {
           'test/acceptance/casper-results.xml' : ['test/casperTests/*_test.js']
         }
       }
+    },
+    express : {
+      options : {
+        //For overriding defaults
+        port: 8080
+      },
+      dev: {
+        //Development server
+        options: {
+          script: 'app.js'
+        }
+      },
+      prod:{
+        //Production server
+        options: {
+          script: 'app.js'
+        }
+      },
+      test:{
+        //Testing server
+        options: {
+          script: 'app.js'
+        }
+      }
+    },
+    watch: {
+      express: {
+        files: ['**/*.js'],
+        tasks: ['express: dev'],
+        options: {
+          spawn: true
+        }
+      }
     }
+
   });
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-casper');
+  grunt.loadNpmTasks('grunt-express-server');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('default', ['jshint', 'casper']);
+  grunt.registerTask('test', ['jshint','express:dev','casper']);
+  grunt.registerTask('server', 'express:dev');
+  grunt.registerTask('default', ['express:dev', 'watch']);
 };
