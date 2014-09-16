7e7f1a795ae9f7aa44e76c15260e37ef04fa60b5
Dale Corns <drcorns@hotmail.com>
2014-09-16 03:54:25 -0700

    changes to casper tests

--- test/casperTests/server_test.js
+++ test/casperTests/server_test.js
@@ -1,19 +1,23 @@
 'use strict';
 /*global casper*/
 
-casper.test.begin('testing the server', 1, function suite(test) {
-
-  casper.start('http://localhost:8080', function() {
-    test.assertHttpStatus(200);
+casper.test.begin('Testing the server', 1, function suite(test) {
+  casper.start('http://localhost:8080/index.html', function() {
+    test.assertHttpStatus(200,'Has 200 status');
+    /*test.assertExists('h1.page-title');
+    test.assertSelectorHasText('h1.page-title', 'Hello');
+    test.assertVisible('footer');*/
+  }).run(function() {
+    test.done();
   });
-
+});
   /*casper.then(function(){
     this.log('and then?');
     this.echo(this.getHTML('body'));
   });*/
 
-  casper.run(function(){
+ /* casper.run(function(){
     test.done();
   });
 
-});
+});*/
